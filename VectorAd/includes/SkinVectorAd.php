d3f8d93958caadded1d39ec5b5c7543d30aa49a9
WikiMANNia <chef@wikimannia.org>
2020-11-07 18:18:06 +0100

    Add age classification meta data

--- VectorAd/includes/SkinVectorAd.php
+++ VectorAd/includes/SkinVectorAd.php
@@ -80,6 +80,12 @@ class SkinVectorAd extends SkinTemplate {
 	public function setupSkinUserCss( OutputPage $out ) {
 		parent::setupSkinUserCss( $out );
 
+		// FSM-Altersklassifizierungssystems: www.altersklassifizierung.de
+		global $wgAgeClassificationMetaName, $wgAgeClassificationMetaContent;
+		if ( !empty( $wgAgeClassificationMetaName ) AND !empty( $wgAgeClassificationMetaContent ) ) {
+			$out->addMeta( $wgAgeClassificationMetaName, $wgAgeClassificationMetaContent );
+		}
+
 		$out->addModuleStyles( [
 			'mediawiki.skinning.interface',
 			'skins.vector.styles',
